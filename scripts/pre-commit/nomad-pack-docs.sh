1e216d15361fbb188ecfd8938f94ce0939331865
Flaudísio Tolentino <flaudisio@gmail.com>
2025-07-29 19:35:05 -0300

    refactor(nomad-pack-docs): validate script arguments

--- scripts/pre-commit/nomad-pack-docs.sh
+++ scripts/pre-commit/nomad-pack-docs.sh
@@ -22,6 +22,11 @@ function remove_temp_files()
 
 function main()
 {
+    if [[ $# -eq 0 ]] ; then
+        echo "Usage: $0 [terraform-docs args] paths..." >&2
+        exit 2
+    fi
+
     trap remove_temp_files EXIT
 
     local tf_docs_args=()
@@ -42,17 +47,20 @@ function main()
     local temp_file
 
     for filepath in "$@" ; do
+        # For now only pack variables files are supported
+        if [[ ! "$filepath" =~ .+/(vars|variables)\.hcl$ ]] ; then
+            continue
+        fi
+
         dir="$( dirname "$filepath" )"
 
         # Hack: 'variables.hcl' files are not loaded by terraform-docs, so we create a temporary
         # '.tf' file to make it work
         # See https://github.com/terraform-docs/terraform-config-inspect/blob/5b88c7ed/tfconfig/load.go#L128
-        if [[ "$filepath" == *variables.hcl ]] ; then
-            temp_file="${filepath}.temp.tf"
-            FILES_TO_REMOVE+=( "$temp_file" )
+        temp_file="${filepath}.temp.tf"
+        FILES_TO_REMOVE+=( "$temp_file" )
 
-            cp "$filepath" "$temp_file"
-        fi
+        cp "$filepath" "$temp_file"
 
         terraform-docs markdown "${tf_docs_args[@]}" --output-mode=inject --output-file=README.md "$dir"
     done
